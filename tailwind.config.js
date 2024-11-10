6852fb019448ce76a684c2a9f29b77ddf05d9435
Christiaan B van Zyl <christiaanvanzyl@gmail.com>
2024-11-10 14:30:12 +0200

    Formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@ module.exports = {
 	// TODO Review this for use with templ.
 	// Just use settings.json as described below?
 	// https://templ.guide/commands-and-tools/ide-support
-  content: ["./www/themes/shopd/**/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./www/themes/shopd/**/*.html"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 }
 
