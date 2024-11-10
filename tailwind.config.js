810ba7a213ff25cb654dfc8e4aa081272ea97f1f
Christiaan B van Zyl <christiaanvanzyl@gmail.com>
2024-11-10 12:22:47 +0200

    Config files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	// TODO Review this for use with templ.
+	// Just use settings.json as described below?
+	// https://templ.guide/commands-and-tools/ide-support
+  content: ["./www/themes/shopd/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
